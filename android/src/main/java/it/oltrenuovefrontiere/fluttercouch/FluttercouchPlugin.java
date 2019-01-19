501f70885711e67bcb1796e0d081ee37a548b0c6
David Alm <sensiblearts@gmail.com>
2019-01-19 16:52:58 -0500

    removed Query and Replicator files

--- android/src/main/java/it/oltrenuovefrontiere/fluttercouch/FluttercouchPlugin.java
+++ android/src/main/java/it/oltrenuovefrontiere/fluttercouch/FluttercouchPlugin.java
@@ -42,8 +42,9 @@ public class FluttercouchPlugin implements MethodCallHandler {
         //final MethodChannel jsonChannel = new MethodChannel(registrar.messenger(), "it.oltrenuovefrontiere.fluttercouchJson", JSONMethodCodec.INSTANCE);
         //jsonChannel.setMethodCallHandler(new FluttercouchPlugin());
 
-        final EventChannel eventChannel = new EventChannel(registrar.messenger(), "it.oltrenuovefrontiere.fluttercouch/replicationEventChannel");
-        eventChannel.setStreamHandler(new ReplicationEventListener(flutterCouchPlugin.mCbManager));
+        // D.A. removed
+        // final EventChannel eventChannel = new EventChannel(registrar.messenger(), "it.oltrenuovefrontiere.fluttercouch/replicationEventChannel");
+        // eventChannel.setStreamHandler(new ReplicationEventListener(flutterCouchPlugin.mCbManager));
     }
 
     @Override
@@ -93,78 +94,78 @@ public class FluttercouchPlugin implements MethodCallHandler {
                     result.error("errGet", "error getting the document with id: " + _id, e.toString());
                 }
                 break;
-            case ("setReplicatorEndpoint"):
-                String _endpoint = call.arguments();
-                try {
-                    String _result = mCbManager.setReplicatorEndpoint(_endpoint);
-                    result.success(_result);
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                    result.error("errURI", "error setting the replicator endpoint uri to " + _endpoint, e.toString());
-                }
-                break;
-            case ("setReplicatorType"):
-                String _type = call.arguments();
-                try {
-                    result.success(mCbManager.setReplicatorType(_type));
-                } catch (CouchbaseLiteException e) {
-                    e.printStackTrace();
-                    result.error("errReplType", "error setting replication type to " + _type, e.toString());
-                }
-                break;
-            case ("setReplicatorBasicAuthentication"):
-                Map<String, String> _auth = call.arguments();
-                try {
-                    result.success(mCbManager.setReplicatorBasicAuthentication(_auth));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    result.error("errAuth", "error setting authentication for replicator", null);
-                }
-                break;
-            case ("setReplicatorSessionAuthentication"):
-                String _sessionID = call.arguments();
-                try {
-                    result.success(mCbManager.setReplicatorSessionAuthentication(_sessionID));
-                } catch (Exception e) {
-                    e.printStackTrace();;
-                    result.error("errAuth", "invalid session ID", null);
-                }
-                break;
-            case ("setReplicatorContinuous"):
-                Boolean _continuous = call.arguments();
-                try {
-                    result.success(mCbManager.setReplicatorContinuous(_continuous));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    result.error("errContinuous", "unable to set replication to continuous", null);
-                }
-                break;
-            case ("initReplicator"):
-                mCbManager.initReplicator();
-                result.success("");
-                break;
-            case ("startReplicator"):
-                mCbManager.startReplicator();
-                result.success("");
-                break;
-            case ("stopReplicator"):
-                mCbManager.stopReplicator();
-                result.success("");
-                break;
-            case ("executeQuery"):
-                HashMap<String, String> _queryMap = call.arguments();
-                Query query = QueryManager.buildFromMap(_queryMap, mCbManager);
-                try {
-                    result.success(query.explain());
-                } catch (CouchbaseLiteException e) {
-                    e.printStackTrace();
-                    result.error("errExecutingQuery", "error executing query ", e.toString());
-                }
-                break;
-            case ("execute"):
-                JSONObject queryJson = call.arguments();
-                Query queryFromJson = new QueryJson(queryJson).toCouchbaseQuery();
-                break;
+            // case ("setReplicatorEndpoint"):
+            //     String _endpoint = call.arguments();
+            //     try {
+            //         String _result = mCbManager.setReplicatorEndpoint(_endpoint);
+            //         result.success(_result);
+            //     } catch (URISyntaxException e) {
+            //         e.printStackTrace();
+            //         result.error("errURI", "error setting the replicator endpoint uri to " + _endpoint, e.toString());
+            //     }
+            //     break;
+            // case ("setReplicatorType"):
+            //     String _type = call.arguments();
+            //     try {
+            //         result.success(mCbManager.setReplicatorType(_type));
+            //     } catch (CouchbaseLiteException e) {
+            //         e.printStackTrace();
+            //         result.error("errReplType", "error setting replication type to " + _type, e.toString());
+            //     }
+            //     break;
+            // case ("setReplicatorBasicAuthentication"):
+            //     Map<String, String> _auth = call.arguments();
+            //     try {
+            //         result.success(mCbManager.setReplicatorBasicAuthentication(_auth));
+            //     } catch (Exception e) {
+            //         e.printStackTrace();
+            //         result.error("errAuth", "error setting authentication for replicator", null);
+            //     }
+            //     break;
+            // case ("setReplicatorSessionAuthentication"):
+            //     String _sessionID = call.arguments();
+            //     try {
+            //         result.success(mCbManager.setReplicatorSessionAuthentication(_sessionID));
+            //     } catch (Exception e) {
+            //         e.printStackTrace();;
+            //         result.error("errAuth", "invalid session ID", null);
+            //     }
+            //     break;
+            // case ("setReplicatorContinuous"):
+            //     Boolean _continuous = call.arguments();
+            //     try {
+            //         result.success(mCbManager.setReplicatorContinuous(_continuous));
+            //     } catch (Exception e) {
+            //         e.printStackTrace();
+            //         result.error("errContinuous", "unable to set replication to continuous", null);
+            //     }
+            //     break;
+            // case ("initReplicator"):
+            //     mCbManager.initReplicator();
+            //     result.success("");
+            //     break;
+            // case ("startReplicator"):
+            //     mCbManager.startReplicator();
+            //     result.success("");
+            //     break;
+            // case ("stopReplicator"):
+            //     mCbManager.stopReplicator();
+            //     result.success("");
+            //     break;
+            // case ("executeQuery"):
+            //     HashMap<String, String> _queryMap = call.arguments();
+            //     Query query = QueryManager.buildFromMap(_queryMap, mCbManager);
+            //     try {
+            //         result.success(query.explain());
+            //     } catch (CouchbaseLiteException e) {
+            //         e.printStackTrace();
+            //         result.error("errExecutingQuery", "error executing query ", e.toString());
+            //     }
+            //     break;
+            // case ("execute"):
+            //     JSONObject queryJson = call.arguments();
+            //     Query queryFromJson = new QueryJson(queryJson).toCouchbaseQuery();
+            //     break;
             default:
                 result.notImplemented();
         }
