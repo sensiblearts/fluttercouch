ecc67d6c33456511acc345e4601668433b910355
David Alm <sensiblearts@gmail.com>
2019-01-19 16:33:17 -0500

    Document 1.4.4 way

--- android/src/main/java/it/oltrenuovefrontiere/fluttercouch/CBManager.java
+++ android/src/main/java/it/oltrenuovefrontiere/fluttercouch/CBManager.java
@@ -1,19 +1,22 @@
 package it.oltrenuovefrontiere.fluttercouch;
 
-import com.couchbase.lite.BasicAuthenticator;
+//import com.couchbase.lite.BasicAuthenticator;
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
-import com.couchbase.lite.DatabaseConfiguration;
+//import com.couchbase.lite.DatabaseConfiguration;
 import com.couchbase.lite.Document;
-import com.couchbase.lite.Endpoint;
-import com.couchbase.lite.LogDomain;
-import com.couchbase.lite.LogLevel;
-import com.couchbase.lite.MutableDocument;
-import com.couchbase.lite.Replicator;
-import com.couchbase.lite.ReplicatorConfiguration;
-import com.couchbase.lite.SessionAuthenticator;
-import com.couchbase.lite.URLEndpoint;
-import com.couchbase.litecore.C4Replicator;
+//import com.couchbase.lite.Endpoint;
+//import com.couchbase.lite.LogDomain;
+//import com.couchbase.lite.LogLevel;
+import com.couchbase.lite.util.Log;
+//import com.couchbase.lite.MutableDocument;
+
+//import com.couchbase.lite.Replicator;
+//import com.couchbase.lite.ReplicatorConfiguration;
+//
+//import com.couchbase.lite.SessionAuthenticator;
+//import com.couchbase.lite.URLEndpoint;
+//import com.couchbase.litecore.C4Replicator;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -23,8 +26,8 @@ import java.util.Map;
 public class CBManager {
     private static final CBManager mInstance = new CBManager();
     private HashMap<String, Database> mDatabase = new HashMap<>();
-    private ReplicatorConfiguration mReplConfig;
-    private Replicator mReplicator;
+//    private ReplicatorConfiguration mReplConfig;
+//    private Replicator mReplicator;
     private String defaultDatabase = "defaultDatabase";
 
     private CBManager() {
@@ -46,15 +49,15 @@ public class CBManager {
     }
 
     public String saveDocument(Map<String, Object> _map) throws CouchbaseLiteException {
-        MutableDocument mutableDoc = new MutableDocument(_map);
-        mDatabase.get(defaultDatabase).save(mutableDoc);
-        return mutableDoc.getId();
+        Document doc = mDatabase.get(defaultDatabase).createDocument();
+        doc.putProperties(_map);
+        return doc.getId();
     }
 
     public String saveDocumentWithId(String _id, Map<String, Object> _map) throws CouchbaseLiteException {
-        MutableDocument mutableDoc = new MutableDocument(_id, _map);
-        mDatabase.get(defaultDatabase).save(mutableDoc);
-        return mutableDoc.getId();
+        Document doc = mDatabase.get(defaultDatabase).getDocument(_id);
+        doc.putProperties(_map);
+        return doc.getId();
     }
 
     public Map<String, Object> getDocumentWithId(String _id) throws CouchbaseLiteException {
@@ -77,71 +80,71 @@ public class CBManager {
         return resultMap;
     }
 
-    public void initDatabaseWithName(String _name) throws CouchbaseLiteException {
-        DatabaseConfiguration config = new DatabaseConfiguration(FluttercouchPlugin.context);
-        if (!mDatabase.containsKey(_name)) {
-            defaultDatabase = _name;
-            // Database.setLogLevel(LogDomain.REPLICATOR, LogLevel.VERBOSE);
-            mDatabase.put(_name, new Database(_name, config));
-        }
-    }
+//    public void initDatabaseWithName(String _name) throws CouchbaseLiteException {
+//        DatabaseConfiguration config = new DatabaseConfiguration(FluttercouchPlugin.context);
+//        if (!mDatabase.containsKey(_name)) {
+//            defaultDatabase = _name;
+//            // Database.setLogLevel(LogDomain.REPLICATOR, LogLevel.VERBOSE);
+//            mDatabase.put(_name, new Database(_name, config));
+//        }
+//    }
 
-    public String setReplicatorEndpoint(String _endpoint) throws URISyntaxException {
-        Endpoint targetEndpoint = new URLEndpoint(new URI(_endpoint));
-        mReplConfig = new ReplicatorConfiguration(mDatabase.get(defaultDatabase), targetEndpoint);
-        return mReplConfig.getTarget().toString();
-    }
+//    public String setReplicatorEndpoint(String _endpoint) throws URISyntaxException {
+//        Endpoint targetEndpoint = new URLEndpoint(new URI(_endpoint));
+//        mReplConfig = new ReplicatorConfiguration(mDatabase.get(defaultDatabase), targetEndpoint);
+//        return mReplConfig.getTarget().toString();
+//    }
 
-    public String setReplicatorType(String _type) throws CouchbaseLiteException {
-        ReplicatorConfiguration.ReplicatorType settedType = ReplicatorConfiguration.ReplicatorType.PULL;
-        if (_type.equals("PUSH")) {
-            settedType = ReplicatorConfiguration.ReplicatorType.PUSH;
-        } else if (_type.equals("PULL")) {
-            settedType = ReplicatorConfiguration.ReplicatorType.PULL;
-        } else if (_type.equals("PUSH_AND_PULL")) {
-            settedType = ReplicatorConfiguration.ReplicatorType.PUSH_AND_PULL;
-        }
-        mReplConfig.setReplicatorType(settedType);
-        return settedType.toString();
-    }
-
-    public String setReplicatorBasicAuthentication(Map<String, String> _auth) throws Exception {
-        if (_auth.containsKey("username") && _auth.containsKey("password")) {
-            mReplConfig.setAuthenticator(new BasicAuthenticator(_auth.get("username"), _auth.get("password")));
-        } else {
-            throw new Exception();
-        }
-        return mReplConfig.getAuthenticator().toString();
-    }
-
-    public String setReplicatorSessionAuthentication(String sessionID) throws Exception {
-        if (sessionID != null) {
-            mReplConfig.setAuthenticator(new SessionAuthenticator(sessionID));
-        } else {
-            throw new Exception();
-        }
-        return mReplConfig.getAuthenticator().toString();
-    }
-
-    public boolean setReplicatorContinuous(boolean _continuous) {
-        mReplConfig.setContinuous(_continuous);
-        return mReplConfig.isContinuous();
-    }
-
-    public void initReplicator() {
-        mReplicator = new Replicator(mReplConfig);
-    }
-
-    public void startReplicator() {
-        mReplicator.start();
-    }
-
-    public void stopReplicator() {
-        mReplicator.stop();
-        mReplicator = null;
-    }
-
-    public Replicator getReplicator() {
-        return mReplicator;
-    }
+//    public String setReplicatorType(String _type) throws CouchbaseLiteException {
+//        ReplicatorConfiguration.ReplicatorType settedType = ReplicatorConfiguration.ReplicatorType.PULL;
+//        if (_type.equals("PUSH")) {
+//            settedType = ReplicatorConfiguration.ReplicatorType.PUSH;
+//        } else if (_type.equals("PULL")) {
+//            settedType = ReplicatorConfiguration.ReplicatorType.PULL;
+//        } else if (_type.equals("PUSH_AND_PULL")) {
+//            settedType = ReplicatorConfiguration.ReplicatorType.PUSH_AND_PULL;
+//        }
+//        mReplConfig.setReplicatorType(settedType);
+//        return settedType.toString();
+//    }
+//
+//    public String setReplicatorBasicAuthentication(Map<String, String> _auth) throws Exception {
+//        if (_auth.containsKey("username") && _auth.containsKey("password")) {
+//            mReplConfig.setAuthenticator(new BasicAuthenticator(_auth.get("username"), _auth.get("password")));
+//        } else {
+//            throw new Exception();
+//        }
+//        return mReplConfig.getAuthenticator().toString();
+//    }
+//
+//    public String setReplicatorSessionAuthentication(String sessionID) throws Exception {
+//        if (sessionID != null) {
+//            mReplConfig.setAuthenticator(new SessionAuthenticator(sessionID));
+//        } else {
+//            throw new Exception();
+//        }
+//        return mReplConfig.getAuthenticator().toString();
+//    }
+//
+//    public boolean setReplicatorContinuous(boolean _continuous) {
+//        mReplConfig.setContinuous(_continuous);
+//        return mReplConfig.isContinuous();
+//    }
+//
+//    public void initReplicator() {
+//        mReplicator = new Replicator(mReplConfig);
+//    }
+//
+//    public void startReplicator() {
+//        mReplicator.start();
+//    }
+//
+//    public void stopReplicator() {
+//        mReplicator.stop();
+//        mReplicator = null;
+//    }
+//
+//    public Replicator getReplicator() {
+//        return mReplicator;
+//    }
 }
